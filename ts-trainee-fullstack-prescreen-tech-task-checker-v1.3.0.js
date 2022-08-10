86d93de938176ab5794b879e55f97b166ea0713a
IvanVTechstack <83820812+IvanVTechstack@users.noreply.github.com>
2022-08-10 22:37:44 +0300

    Update and rename ts-trainee-fullstack-prescreen-tech-task-checker-v1.2.6.js to ts-trainee-fullstack-prescreen-tech-task-checker-v1.3.0.js

--- ts-trainee-fullstack-prescreen-tech-task-checker-v1.3.0.js
+++ ts-trainee-fullstack-prescreen-tech-task-checker-v1.3.0.js
@@ -18,25 +18,60 @@ window.showNotification = (notificationText, textColor = 'white') => {
 }
 
 window.showCheckResult = () => {
-    const baseSelector = "span.jest-lite-report__summary-status"
-    let resultNode = window.document.body.querySelector(baseSelector + "--fail")
-    if (resultNode) {
-      console.log("==check result: " + resultNode.innerText)
-      const regexRes = resultNode.innerText.match(/(\d*) passed/)
-      if (regexRes[1]) {
-        const passedTestsCount = parseInt(regexRes[1])
-        console.log("==check passedTestsCount: " + passedTestsCount)
-        if (passedTestsCount >= 3) {
-          console.log("==check PASSED PARTIALLY")
-          window.showNotification("PASSED PARTIALLY", "yellow")
-        } else {
-          console.log("==check FAILED")
-          window.showNotification("FAILED", "red")
-        }
+    const baseSelectors = window.document.body.querySelectorAll('.jest-lite-report__result');
+   
+    const {
+      numberRequiredFail,
+      numberRequiredPass,
+      numberNotRequiredFail,
+      numberNotRequiredPass,
+      testAmount
+    } = window.Array.from(baseSelectors).reduce((total, item) => {
+      const content = item.textContent;
+      const isRequired = content.includes('Required');
+      const isPassed = content.includes('PASS');
+      const isFailed = content.includes('FAIL');
+
+
+      if (isRequired && isFailed) {
+        total.numberRequiredFail = total.numberRequiredFail + 1;
+        return total;
       }
-    } else if (window.document.body.querySelector(baseSelector + "--pass")) {
-      console.log("==check PASSED")
-      window.showNotification("PASSED", "green")
+
+      if (isRequired && isPassed) {
+        total.numberRequiredPass = total.numberRequiredPass + 1;
+        return total;
+      }
+
+      if (isFailed) {
+        total.numberNotRequiredFail = total.numberNotRequiredFail + 1;
+        return total;
+      }
+
+      if (isPassed) {
+        total.numberNotRequiredPass = total.numberNotRequiredPass + 1;
+        return total;
+      }
+      
+      return total;
+
+    }, {
+        testAmount: window.Array.from(baseSelectors).length,
+        numberRequiredFail: 0,  
+        numberRequiredPass: 0,  
+        numberNotRequiredFail: 0, 
+        numberNotRequiredPass: 0, 
+    });
+  
+   
+    if (numberRequiredFail === 0 && numberNotRequiredFail === 0) {
+       window.showNotification("PASSED IDEALLY", "green")
+    } else if (numberRequiredFail > 1) {
+       window.showNotification("FAILED", "red")
+    } else if (numberRequiredFail === 0 && numberNotRequiredFail <= 3) {
+       window.showNotification("PASSED", "green")
+    } else {
+       window.showNotification("PASSED PARTIALLY", "yellow")
     }
   }
 
@@ -44,8 +79,195 @@ window.showCheckResult = () => {
     Tests
   ***********************************************/
 window.tsTestsRun = () => {
+  console.log("run 'Basic tests' tests")
+  describe("'Required tests' function", () => {
+     // BASIC
+    it("should pass basic case #1", () => {
+      const pricing2 = {
+        Designer: {
+          salary: 6000,
+          tax: "20%",
+        },
+        ProjectManager: {
+          salary: 500,
+          tax: "50%",
+        },
+      }
+      const team2 = [
+        {
+          specialization: "ProjectManager",
+          name: "Vasa",
+        },
+        {
+          specialization: "Designer",
+          name: "Dima",
+        },
+        {
+          specialization: "Designer",
+          name: "Sasha",
+        },
+        {
+          specialization: "Designer",
+          name: "Peter",
+        },
+      ]
+      
+      expect(window.calculateTeamFinanceReport(pricing2, team2)).toEqual(
+      {"totalBudgetDesigner": 22500, "totalBudgetProjectManager": 1000, "totalBudgetTeam": 23500})
+    })
+
+    it("should pass basic case #2", () => {
+      const pricing3 = {
+        Progger: {
+          salary: 300, // without decimal counting system (100.5 or 99.99)
+          tax: "30%", // max - 99 min - 0
+        },
+        Designer: {
+          salary: 600,
+          tax: "30%",
+        },
+        ProjectManager: {
+          salary: 500,
+          tax: "90%",
+        },
+      }
+      const team3 = [
+        {
+          specialization: "Progger",
+          name: "Vasa",
+        },
+        {
+          specialization: "Designer",
+          name: "Vika",
+        },
+        {
+          specialization: "ProjectManager",
+          name: "John",
+        },
+        {
+          specialization: "Progger",
+          name: "Lena",
+        },
+      ]
+
+      expect(window.calculateTeamFinanceReport(pricing3, team3)).toEqual(
+      {"totalBudgetDesigner": 857, "totalBudgetProgger": 857, "totalBudgetProjectManager": 5000, "totalBudgetTeam": 6714}
+      )
+    })
+    
+    it("should pass task explanation case", () => {
+      const pricing = {
+        Progger: {
+          // specialization type 'Progger'
+          salary: 1000, // salary minus tax; should be integer; min: 100, max: 100000
+          tax: "15%", // tax percent; presented as string with template `{tax}%` where 'tax' is integer;  min: "0%", max: "99%"
+        },
+        Tester: {
+          salary: 1000,
+          tax: "10%",
+        },
+      }
+      const team = [
+        {
+          name: "Masha", // name of team member
+          specialization: "Progger", // specialization should be picked from `salaries` otherwise member should be ignored in report
+        },
+        {
+          name: "Vasya",
+          specialization: "Tester",
+        },
+        {
+          name: "Taras",
+          specialization: "Tester",
+        },
+      ]
+      
+      expect(window.calculateTeamFinanceReport(pricing, team)).toEqual({
+        totalBudgetTeam: 3398,
+        totalBudgetProgger: 1176,
+        totalBudgetTester: 2222,
+      })
+    })
+
+    it("should pass task example #1", () => {
+      const pricing = {
+        Manager: { salary: 1000, tax: "10%" },
+        Designer: { salary: 600, tax: "30%" },
+        Artist: { salary: 1500, tax: "15%" },
+      }
+      const team = [
+        { name: "Misha", specialization: "Manager" },
+        { name: "Max", specialization: "Designer" },
+        { name: "Vova", specialization: "Designer" },
+        { name: "Leo", specialization: "Artist" },
+      ]
+
+      expect(window.calculateTeamFinanceReport(pricing, team)).toEqual({
+        totalBudgetTeam: 4590,
+        totalBudgetManager: 1111,
+        totalBudgetDesigner: 1714,
+        totalBudgetArtist: 1764,
+      })
+    })
+
+    it("Required should pass task example #2", () => {
+      const salaries = {
+         TeamLead: { salary: 1000, tax: "99%" },
+         Architect: { salary: 9000, tax: "34%" },}
+      const team = [
+         { name: "Alexander", specialization: "TeamLead" },
+         { name: "Gaudi", specialization: "Architect" },
+         { name: "Koolhas", specialization: "Architect" },
+         { name: "Foster", specialization: "Architect" },
+         { name: "Napoleon", specialization: "General" },]
+
+      expect(window.calculateTeamFinanceReport(salaries, team)).toEqual(
+        {"totalBudgetTeam":140909,"totalBudgetTeamLead":100000,"totalBudgetArchitect":40909}
+      )
+    })
+    // BASIC
+   })
+  
   console.log("run 'calculateTeamFinanceReport' tests")
   describe("'calculateTeamFinanceReport' function", () => {
+    it("should skip unrelevant input data", () => {
+      const pricing2 = {
+        Progger: {
+          salary: 100, // without decimal counting system (100.5 or 99.99)
+          tax: "10%", // max - 99 min - 0
+        },
+        Designer: {
+          salary: 6000,
+          tax: "20%",
+        },
+        ProjectManager: {
+          salary: 500,
+          tax: "50%",
+        },
+        ProductManager: {
+          salary: 900,
+          tax: "10%",
+        },
+        Tester: {
+          salary: 100,
+          tax: "20%",
+        },
+      }
+      const team2 = [
+        {
+          specialization: "ProjectManager",
+          name: "Vasa",
+        },
+        {
+          specialization: "Pidor",
+          name: "Putin",
+        },
+      ]
+      
+      expect(window.calculateTeamFinanceReport(pricing2, team2)).toEqual(
+      {"totalBudgetProjectManager": 1000, "totalBudgetTeam": 1000})
+    })
+    
     it("should pass Dima's test #1 (rounding trap)", () => {
       console.log("run Dima's test #1 (rounding trap")
       const pricing1 = {
@@ -84,116 +306,9 @@ window.tsTestsRun = () => {
         {totalBudgetProgger: 800000, totalBudgetTeam: 800000}
       )
     })
-
-    it("should pass Dima's test #2", () => {
-      console.log("run Dima's test #2")
-      const pricing2 = {
-        Progger: {
-          salary: 100000, // without decimal counting system (100.5 or 99.99)
-          tax: "5%", // max - 99 min - 0
-        },
-        Designer: {
-          salary: 600,
-          tax: "20%",
-        },
-        ProjectManager: {
-          salary: 500,
-          tax: "90%",
-        },
-        ProductManager: {
-          salary: 500,
-          tax: "40%",
-        },
-        Tester: {
-          salary: 100,
-          tax: "10%",
-        },
-        Driver: {
-          salary: 99999,
-          tax: "20%",
-        },
-        Architect: {
-          salary: 200,
-          tax: "2%",
-        },
-      }
-      const team2 = [
-        {
-          specialization: "ProjectManager",
-          name: "Vasa",
-        },
-        {
-          specialization: "Designer",
-          name: "Dima",
-        },
-        {
-          specialization: "Designer",
-          name: "Sasha",
-        },
-        {
-          specialization: "Designer",
-          name: "Peter",
-        },
-        {
-          specialization: "Architect",
-          name: "John",
-        },
-        {
-          specialization: "Driver",
-          name: "Nastya",
-        },
-      ]
-      expect(window.calculateTeamFinanceReport(pricing2, team2)).toEqual(
-        {"totalBudgetArchitect": 204, "totalBudgetDesigner": 2250, "totalBudgetDriver": 124998, "totalBudgetProjectManager": 5000, "totalBudgetTeam": 132452}
-      )
-    })
-
-    it("should pass Dima's test #3", () => {
-      console.log("run Dima's test #3")
-      const pricing3 = {
-        Progger: {
-          salary: 999213, // without decimal counting system (100.5 or 99.99)
-          tax: "3%", // max - 99 min - 0
-        },
-        Designer: {
-          salary: 600,
-          tax: "20%",
-        },
-        ProjectManager: {
-          salary: 500,
-          tax: "90%",
-        },
-        PductManager: {
-          salary: 100000,
-          tax: "20%",
-        },
-      }
-      const team3 = [
-        {
-          specialization: "Progger",
-          name: "Vasa",
-        },
-        {
-          specialization: "Designer",
-          name: "Vika",
-        },
-        {
-          specialization: "ProjectManager",
-          name: "John",
-        },
-        {
-          specialization: "Progger",
-          name: "Lena",
-        },
-      ]
-
-      expect(window.calculateTeamFinanceReport(pricing3, team3)).toEqual(
-       {"totalBudgetDesigner": 750, "totalBudgetProgger": 2060232, "totalBudgetProjectManager": 5000, "totalBudgetTeam": 2065982}
-      )
-    })
-
-    it("should pass Dima's test #4 (tricky rounding case with big values)", () => {
-      console.log("run Dima's test #4 (tricky rounding case with big values)")
+    
+    it("should pass Dima's test #2 (tricky rounding case with big values)", () => {
+      console.log("should pass Dima's test #2 (tricky rounding case with big values)")
       const salaries = {
         Progger: {
           salary: 99990000,
@@ -206,10 +321,6 @@ window.tsTestsRun = () => {
         ProjectManager: {
           salary: 73124512,
           tax: "2%",
-        },
-        ProductManager: {
-          salary: 500,
-          tax: "26%",
         },
         Tester: {
           salary: 100,
@@ -233,89 +344,10 @@ window.tsTestsRun = () => {
           specialization: "Tester",
           name: "Dima",
         },
-        {
-          specialization: "General",
-          name: "Napoleon",
-        },
       ]
 
       expect(window.calculateTeamFinanceReport(salaries, team)).toEqual(
         {"totalBudgetDesigner": 606, "totalBudgetProgger": 1999800000, "totalBudgetProjectManager": 74616848, "totalBudgetTeam": 2074417581, "totalBudgetTester": 126}
-      )
-    })
-
-    it("should pass task explanation case", () => {
-      console.log("run task explanation case")
-      const pricing = {
-        Progger: {
-          // specialization type 'Progger'
-          salary: 1000, // salary minus tax; should be integer; min: 100, max: 100000
-          tax: "15%", // tax percent; presented as string with template `{tax}%` where 'tax' is integer;  min: "0%", max: "99%"
-        },
-        Tester: {
-          salary: 1000,
-          tax: "10%",
-        },
-      }
-
-      const team = [
-        {
-          name: "Masha", // name of team member
-          specialization: "Progger", // specialization should be picked from `salaries` otherwise member should be ignored in report
-        },
-        {
-          name: "Vasya",
-          specialization: "Tester",
-        },
-        {
-          name: "Taras",
-          specialization: "Tester",
-        },
-      ]
-
-      expect(window.calculateTeamFinanceReport(pricing, team)).toEqual({
-        totalBudgetTeam: 3398,
-        totalBudgetProgger: 1176,
-        totalBudgetTester: 2222,
-      })
-    })
-
-    it("should pass task example case #1", () => {
-      console.log("run task example case #1")
-      const pricing = {
-        Manager: { salary: 1000, tax: "10%" },
-        Designer: { salary: 600, tax: "30%" },
-        Artist: { salary: 1500, tax: "15%" },
-      }
-      const team = [
-        { name: "Misha", specialization: "Manager" },
-        { name: "Max", specialization: "Designer" },
-        { name: "Vova", specialization: "Designer" },
-        { name: "Leo", specialization: "Artist" },
-      ]
-
-      expect(window.calculateTeamFinanceReport(pricing, team)).toEqual({
-        totalBudgetTeam: 4590,
-        totalBudgetManager: 1111,
-        totalBudgetDesigner: 1714,
-        totalBudgetArtist: 1764,
-      })
-    })
-
-    it("should pass task example case #2", () => {
-      console.log("run task example case #2")
-      const salaries = {
-         TeamLead: { salary: 1000, tax: "99%" },
-         Architect: { salary: 9000, tax: "34%" },}
-      const team = [
-         { name: "Alexander", specialization: "TeamLead" },
-         { name: "Gaudi", specialization: "Architect" },
-         { name: "Koolhas", specialization: "Architect" },
-         { name: "Foster", specialization: "Architect" },
-         { name: "Napoleon", specialization: "General" },]
-
-      expect(window.calculateTeamFinanceReport(salaries, team)).toEqual(
-        {"totalBudgetTeam":140909,"totalBudgetTeamLead":100000,"totalBudgetArchitect":40909}
       )
     })
     
